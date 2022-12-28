d466f3a7accd58c580cd3ef923bdecec15bd2a83
RagOfJoes <apps@ragofjoes.dev>
2022-12-27 23:48:39 -0800

    chore: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import solid from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  adapter: vercel(),
   integrations: [solid(), tailwind()],
+  output: 'server',
 });
