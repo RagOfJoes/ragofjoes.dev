1fa6170a5228814faaae9fc089cbf56e65dd58ca
RagOfJoes <apps@ragofjoes.dev>
2022-12-27 23:45:15 -0800

    feat: add projects array

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,6 +2,61 @@ import dayjs from 'dayjs';
 import type { IconTypes } from 'solid-icons';
 import { ImGithub, ImLinkedin2, ImTwitter } from 'solid-icons/im';
 
+export const PROJECTS = [
+  {
+    description:
+      'A puzzle game that was inspired by the BBC’s “Only Connect” game show. Users create a puzzle with 16 words where each group of 4 words have some connection between them. Players attempt to link each group together and guess each connection.',
+    image: '/puzzlely-preview.webp',
+    tags: [
+      'Typescript',
+      'React',
+      'Next',
+      'Go',
+      'OAuth2',
+      'MySQL',
+      'Docker',
+      'Terraform',
+      'DigitalOcean',
+    ],
+    title: 'Puzzlely',
+    url: 'https://www.puzzlely.io/',
+  },
+  {
+    description:
+      'An iOS library that extracts the ColorTheme from an image. It uses the Octree Color Quantization Algorithm to build the image’s distinct color palette.',
+    image: '/octreePalette-preview.webp',
+    tags: ['Mobile', 'iOS', 'Swift 5.0'],
+    title: 'OctreePalette',
+    url: 'https://github.com/RagOfJoes/OctreePalette',
+  },
+  {
+    description:
+      'A web app that allows users to create, import, and share their favorite recipes. Users can also create “Creations” that show off their attempts at following other user generated recipes.',
+    image: '/spoonfed-preview.webp',
+    tags: [
+      'React',
+      'Next',
+      'NodeJS',
+      'GraphQL',
+      'OAuth2',
+      'OIDC',
+      'Redis',
+      'MongoDB',
+      'Azure',
+    ],
+    title: 'Spoonfed',
+    url: 'https://spoonfed.dev/',
+  },
+  {
+    description:
+      'A simple identity provider that is build on top of the OIDC protocol for authentication. Users can manage their profile and active sessions.',
+    image: '/identityProvider-preview.webp',
+    tags: ['NodeJS', 'OAuth2', 'OIDC', 'MongoDB', 'Redis'],
+    title: 'Identity Provider',
+    url: 'https://spoonfed.dev/api/login',
+  },
+];
+
 export const ROUTES = [
   { href: '/', title: 'ABOUT' },
   { href: '/projects', title: 'PROJECTS' },
