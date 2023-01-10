cd46c8d3e34b30eb2306813874803d3015e90158
RagOfJoes <apps@ragofjoes.dev>
2023-01-10 12:08:35 -0800

    chore: add sitemap
    * update deps
    * move @astro/* to dependencies
    * add sitemap integration into astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
-/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/no-unresolved */
+import sitemap from '@astrojs/sitemap';
 import solid from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  integrations: [solid(), tailwind()],
+  integrations: [
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
+    solid(),
+    tailwind(),
+  ],
   site: 'https://www.ragofjoes.dev',
 });
