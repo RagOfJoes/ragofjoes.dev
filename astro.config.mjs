bb98d84f038fc1bf6d4e2d005a9f9923728bfbad
RagOfJoes <apps@ragofjoes.dev>
2023-04-13 14:12:14 -0700

    feat: add vercel astro integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,24 @@
 /* eslint-disable import/no-unresolved */
-import sitemap from '@astrojs/sitemap';
-import solid from '@astrojs/solid-js';
-import tailwind from '@astrojs/tailwind';
+import sitemap from "@astrojs/sitemap";
+import solid from "@astrojs/solid-js";
+import tailwind from "@astrojs/tailwind";
+import vercelStatic from "@astrojs/vercel/static";
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    sitemap({
-      changefreq: 'weekly',
-      priority: 0.7,
-      lastmod: new Date(),
-    }),
-    solid(),
-    tailwind(),
-  ],
-  site: 'https://www.ragofjoes.dev/',
+	integrations: [
+		sitemap({
+			changefreq: "weekly",
+			priority: 0.7,
+			lastmod: new Date(),
+		}),
+		solid(),
+		tailwind(),
+		vercelStatic({
+			analytics: true,
+		}),
+	],
+	site: "https://www.ragofjoes.dev/",
 });
