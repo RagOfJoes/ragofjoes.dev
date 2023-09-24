a4bf53e90d770fa4acd4024e3cbf348c3a526d43
RagOfJoes <apps@ragofjoes.dev>
2023-09-24 13:40:44 -0700

    chore: add OffsetTent to projects

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,6 +5,14 @@ import { ImGithub, ImLinkedin2, ImTwitter } from "solid-icons/im";
 export const PROJECTS = [
 	{
 		description:
+			"Offset Tent is a tool that synchronizes your Basecamp cards and GitHub issues. It allows you to keep your team up to date with the latest changes made to your codebases.",
+		image: "/offsetTent-preview.webp",
+		tags: ["Go", "OAuth2", "MySQL", "Docker", "GitHub Actions"],
+		title: "Offset Tent",
+		url: "https://tent.offsetdevops.com/",
+	},
+	{
+		description:
 			"A puzzle game that was inspired by the BBC’s “Only Connect” game show. Users create a puzzle with 16 words where each group of 4 words have some connection between them. Players attempt to link each group together and guess each connection.",
 		image: "/puzzlely-preview.webp",
 		tags: [
@@ -85,34 +93,23 @@ export const WORK_EXPERIENCE = [
 	{
 		company: "Offset",
 		description: [
-			"Led development team in creating a low code solution allowing our clients to easily integrate our Cart and CMS service into their existing site",
-			"Refactored legacy PHP MVC framework to improve integration with React which decreased the number of duplicate and brittle code by up to 60%",
-			"Implemented Docker into our code to decrease the amount of variability between environments and simplify local development setup",
-			"Introduced a proper Version Control workflow with GitHub which cut down production bugs by 80%",
+			"Spearheaded the creation of a low coded solution, enabling clients to seamlessly integrate our Cart and CMS services into their pre-existing websites",
+			"Refactored legacy PHP MVC framework to enhance compatibility with React, resulting in a reduction of duplicated and fragile code by up to 60%",
+			"Rewrote and implemented frontend build tools to products cutting down compilation times by up to 200%. Enhanced security measures were implemented, uncovering known vulnerabilities within frontend dependencies",
+			"Closely collaborated with both the Project Manager and CTO to expand and strengthen our development team",
+			"Integrated Docker into our codebase to reduce environmental discrepancies and streamline the setup process for local development environments",
 		],
 		end: dayjs(),
 		job: "Product Engineer",
-		link: "https://www.offsetpartners.com/",
-		start: dayjs("12/08/2020", "MM/DD/YYYY"),
-	},
-	{
-		company: "Offset",
-		description: [
-			"Managed e-commerce platform by squashing bugs and addressing client requests",
-			"Prototyped several internal tools such as a centralized Identity Provider, GraphQL wrapper for our Cart service, and a real-time client issue tracker",
-			"Worked closely with designers to build unique features for Client websites",
-		],
-		end: dayjs("12/08/2020", "MM/DD/YYYY"),
-		job: "JR. Software Developer",
 		link: "https://www.offsetpartners.com/",
 		start: dayjs("07/07/2019", "MM/DD/YYYY"),
 	},
 	{
 		company: "Napa Valley College",
 		description: [
-			"Assisted classmates with various studying techniques for exams, projects, and/or, assignments",
+			"Assisted fellow classmates by offering guidance on diverse study techniques for exams, projects, and assignments",
 			"Presented different ways to visualize, understand, and enjoy complex programming topics",
-			"Collaborated with professors to address teaching and assignment issues",
+			"Closely collaborated with professors to address teaching and assignment related concerns, fostering an improved learning experience",
 		],
 		end: dayjs("05/21/2019", "MM/DD/YYYY"),
 		job: "Computer Science Tutor",
