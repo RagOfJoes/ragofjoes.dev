c5e77e832f1ab0ca1f0edf2200629b0d02768dbc
RagOfJoes <dev@ragofjoes.dev>
2024-11-25 07:24:45 -0800

    fix: case insensitive thingy

--- src/components/dialog/use-dialog-context.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from "@/lib/create-context";
-
-import { UseDialog } from "./use-dialog";
-
-export const [DialogProvider, useDialogContext] = createContext<UseDialog>({
-	name: "DialogContext",
-	strict: true,
-});
