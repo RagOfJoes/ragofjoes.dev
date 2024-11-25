f541cdbbb132ae50f95bcaf5250f192a56924a8c
RagOfJoes <dev@ragofjoes.dev>
2024-11-25 07:22:42 -0800

    fix: case insensitive thingy

--- src/components/carousel/use-carousel-context.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from "@/lib/create-context";
-
-import { UseCarousel } from "./use-carousel";
-
-export const [CarouselProvider, useCarouselContext] =
-	createContext<UseCarousel>({
-		name: "CarouselContext",
-		strict: true,
-	});
