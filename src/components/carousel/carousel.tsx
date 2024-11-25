e3cb8c330e888589b1bde22812fbbf5af9775a27
RagOfJoes <dev@ragofjoes.dev>
2024-11-25 07:24:27 -0800

    fix: case insensitive thingy

---
+++ src/components/carousel/carousel.tsx
@@ -0,0 +1,62 @@
+import { For, Show } from "solid-js";
+
+import { Presence } from "@motionone/solid";
+
+import { Rerun } from "@/components/rerun";
+import { addEventListener } from "@/lib/add-event-listener";
+import { PROJECTS } from "@/lib/constants";
+
+import { CarouselSlide } from "./carousel-slide";
+import { useCarousel } from "./use-carousel";
+import { CarouselProvider } from "./use-carousel-context";
+
+export type CarouselProps = {
+	slides: typeof PROJECTS;
+};
+
+export function Carousel(props: CarouselProps) {
+	const [state, actions] = useCarousel(props);
+
+	if (!import.meta.env.SSR) {
+		addEventListener(window, "keydown", (e: KeyboardEvent) => {
+			switch (e.key) {
+				case "h":
+				case "ArrowLeft":
+					actions.previous();
+					break;
+				case "l":
+				case "ArrowRight":
+					actions.next();
+					break;
+				default:
+					break;
+			}
+		});
+	}
+
+	return (
+		<CarouselProvider value={[state, actions]}>
+			<div class="relative h-screen pt-32">
+				<Presence initial={false} exitBeforeEnter>
+					<Rerun on={state.current()}>
+						<div class="relative h-full">
+							<For each={props.slides}>
+								{(slide, i) => (
+									<Show when={i() === state.current()}>
+										<CarouselSlide
+											url={slide.url}
+											tags={slide.tags}
+											image={slide.image}
+											title={slide.title}
+											description={slide.description}
+										/>
+									</Show>
+								)}
+							</For>
+						</div>
+					</Rerun>
+				</Presence>
+			</div>
+		</CarouselProvider>
+	);
+}
