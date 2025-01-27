16a21b074643824e4106803526454a29128b04f5
RagOfJoes <dev@ragofjoes.dev>
2025-01-26 22:49:01 -0800

    chore: remove unused fields

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,4 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
@@ -23,19 +24,6 @@ module.exports = {
 					DEFAULT: "hsl(48.65deg 100% 85.49% / <alpha-value>)",
 					foreground: "hsl(60deg 6.67% 5.88% / <alpha-value>)",
 				},
-
-				"rsp-base": "hsl(249deg 22% 12% / <alpha-value>)",
-				"rsp-foam": "hsl(189deg 43% 73% / <alpha-value>)",
-				"rsp-gold": "hsl(35deg 88% 72% / <alpha-value>)",
-				"rsp-iris": "hsl(267deg 57% 78% / <alpha-value>)",
-				"rsp-love": "hsl(343deg 76% 68% / <alpha-value>)",
-				"rsp-muted": "hsl(249deg 12% 47% / <alpha-value>)",
-				"rsp-overlay": "hsl(248deg 25% 18% / <alpha-value>)",
-				"rsp-pine": "hsl(197deg 49% 38% / <alpha-value>)",
-				"rsp-rose": "hsl(2deg 55% 83% / <alpha-value>)",
-				"rsp-subtle": "hsl(248deg 15% 61% / <alpha-value>)",
-				"rsp-surface": "hsl(247deg 23% 15% / <alpha-value>)",
-				"rsp-text": "hsl(245deg 50% 91% / <alpha-value>)",
 			},
 			keyframes: {
 				"scroll-down": {
